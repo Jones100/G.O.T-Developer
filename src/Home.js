b9ed0e1d372da26f94eb164aa27d48565dfb662d
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-10 15:49:42 +0100

    added 2 more images

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,41 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 class Home extends Component {
 	constructor(props) {
 		super(props);
-			this.state = {};
-    }
-
-	render() {
-		return (
-			<div className="main-container">
+			this.state = {
+			};
+	}
+		render() {
+			return (
+				<div className="main-container">
+					
 					<div className="main-header-container">
-							<div className="main-header"> What type of developer are you?? </div>
+						<div className="main-header"> What type of developer are you?? </div>
+					</div>
+				
+					<div className="main-body">
+						<div className="character-wheel">
+							
+							<div className="img-container">
+								<div className="littlefinger-img"> </div>
+							</div>	
+							
+							<div className="middle-container">
+								<div className="img-container">
+									<div className="brienne-img"> </div>
+								</div>	
+							</div>
+
+							<div className="img-container">
+								<div className="tyrion-img"> </div>
+							</div>	
+							
+						
+						
+						</div>
+				
 
 					</div>
-
-					<div className="character-wheel">
-
-
-
-					</div>
-
+			
 			</div>
 		);
 	}
