f8c49df2f12c456ceed8077bea054b3eb7ad1395
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-16 18:17:05 +0100

    added Littlefinger pages and wrote text

---
+++ src/Littlefinger.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Littlefinger.css"
+
+class Littlefinger extends Component {
+	constructor(props) {
+		super(props);
+			this.state = {
+			};
+	}
+    render() {
+			return ( 
+				<div className="littlefinger-text"> 
+					A master manipulator of software! You have truly conquered the art of non-conformity
+          and having code bend to your vision and will! The type to give text-book traditions
+					the middle finger and ruthlessly banish old browsers for modern up to date ones.
+					This type of developer may not sepearate their HTML and CSS content, not consider
+					their fellow team-mates in keeping their code universally easy to follow and perhaps
+					break all trust by relentless office back-stabbing on their climb to the top! 
+					Allbeit a superb and savvy developer! 	
+				</div>
+        );
+    }
+}
+
+export default Littlefinger;
