7f4d5378f6eb655b1b11a8da73e46611b3bb684b
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-16 19:17:46 +0100

    added Brienne pages and wrote text

---
+++ src/Brienne.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Brienne.css"
+
+class Brienne extends Component {
+	constructor(props) {
+		super(props);
+			this.state = {
+			};
+	}
+    render() {
+			return ( 
+				<div className="brienne-text"> 
+					Enter Brienne...protector of the laws of web development, loyal to their teammates almost to a fault,
+					unwavering in their bold and rigid path to serve humanity and deliver clean, tried and tested code!
+				  They prefer Java to JavaScript cause of it's strict and set structure, untrusting of any new technology, 
+					opting to stick to HTML 3.0 and Bootstrap 2.0! They more or less breathe depreciation! They are however
+					great in their chosen fields - whether it's client-side, server-side, full-stack etc and will get the 
+					job done! Even if it is a blast from the past and takes 40 lines of definite, solidified code rather 
+					than 20 slighly looser lines :-);
+				</div>
+        );
+    }
+}
+
+export default Brienne;
