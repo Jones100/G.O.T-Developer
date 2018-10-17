dc7d9fba5b86449641bd32b0ab9287c729887d28
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-17 16:08:07 +0100

    added Tywin pages and wrote text

---
+++ src/Tywin.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Tywin extends Component {
+	constructor(props) {
+		super(props);
+			this.state = {
+			};
+	}
+	render() {
+		return (
+			<div className="tywin-text">
+				Another sucker for the old school...These are no suckers pardon my English, they're as tough as they come!!
+				A stern gaze and steely manner make up these folk...and usually loads of greys (full of wisdom of course). 
+				Usually found spearheading the company - on the board of directors or owning the joint, these guys are
+				tunneled in their vision and zealous in ambition! Although they do exercise tradition and are quite
+				intolerable to newer technologies/concepts like React and AI, naturally preferring remote work (as they're
+				lone wolves through and through) with a rusty desktop that does the job. They refuse to take blame or
+				extend their name alongside percieved failures as their reputation is their world! 
+				Remember to smile and be nice...we've all what happens to you in the series :-);
+			</div>
+		);
+}			
+}
+
+export default Tywin;
