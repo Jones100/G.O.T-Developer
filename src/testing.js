df354184367144ce51af3f504839359faf36af6d
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-23 01:44:39 +0100

    added page to test image/text structure

---
+++ src/testing.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./Text.css";
+
+class Testing extends Component {
+	constructor(props) {
+		super(props);
+			this.state = {
+			};
+	}
+	render() {
+		return ( 
+			<div className="varys-main-container">
+
+				<div className="varys-img-container">
+					<div className="varys-text-img"> </div>
+				</div>
+				<div className="varys-text"> 
+                   Ohh, what an oddball indeed...these 'creatures' are found in their lairs keenly engulfing the JavaScript secrets or
+					at the rear of web seminars intently devouring info and feeding their bottomless thirst for knowledge - 
+					particularly the dark arts. Master strategists it must be said but wierdly aberrant and introverted. These developers
+					will be alone, they prefer their workmates to let them get on with it and flourish in languages such as BrainF**k and
+					C++. They're not quite as recognized for their efforts as they'd like or even deserve but their awkward social
+					display and coffee-stained ripped black hoodie are something of a repeller. They secretly wish for a friend, a hug
+					and an 'F YOU!' to the world 'if' they rise to the top...
+                </div>
+			
+	
+			</div>
+			);
+	}
+}
+
+export default Testing;
