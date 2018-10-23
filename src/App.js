cb8217266de7c9e3bddd7bd7a58d551342bfb39e
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-23 02:02:34 +0100

    deleted some files, added others

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 import Home from "./Home.js";
 import "./Home.css";
+import "./Text.css";
+import Testing from "./testing.js";
+import Littlefinger from "./Littlefinger.js";
+import Brienne from "./Brienne.js";
+import Tyrion from "./Tyrion.js";
+import Tywin from "./Tywin.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
@@ -15,6 +21,7 @@ class App extends Component {
       <Router>
         <div className="app">
           <Route exact path="/" component={Home} />
+          <Route exact path="/testing" component={Testing} />
         </div>
       </Router>
     );
