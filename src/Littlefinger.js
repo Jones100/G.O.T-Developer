3bfc397e5fb607ced1e5db22e93776c595371fc7
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-23 04:48:44 +0100

    littlefinger text/img structural changes

--- src/Littlefinger.js
+++ src/Littlefinger.js
@@ -10,7 +10,7 @@ class Littlefinger extends Component {
 			return ( 
 				<div className="littlefinger-text"> 
 					A master manipulator of software! You have truly conquered the art of non-conformity
-          and having code bend to your vision and will! The type to give textbook traditions
+          			and having code bend to your vision and will! The type to give textbook traditions
 					the middle finger and ruthlessly banish old browsers for modern up to date ones.
 					This type of developer may not sepearate their HTML and CSS content, not consider
 					their fellow team-mates in keeping their code universally easy to follow and perhaps
--- src/testing.js
+++ src/testing.js
@@ -9,19 +9,19 @@ class Testing extends Component {
 	}
 	render() {
 		return ( 
-			<div className="varys-main-container">
+			<div className="littlefinger-main-container">
 
-				<div className="varys-img-container">
-					<div className="varys-text-img"> </div>
+				<div className="littlefinger-img-container">
+					<div className="littlefinger-text-img"> </div>
 				</div>
-				<div className="varys-text"> 
-                   Ohh, what an oddball indeed...these 'creatures' are found in their lairs keenly engulfing the JavaScript secrets or
-					at the rear of web seminars intently devouring info and feeding their bottomless thirst for knowledge - 
-					particularly the dark arts. Master strategists it must be said but wierdly aberrant and introverted. These developers
-					will be alone, they prefer their workmates to let them get on with it and flourish in languages such as BrainF**k and
-					C++. They're not quite as recognized for their efforts as they'd like or even deserve but their awkward social
-					display and coffee-stained ripped black hoodie are something of a repeller. They secretly wish for a friend, a hug
-					and an 'F YOU!' to the world 'if' they rise to the top...
+				<div className="littlefinger-text"> 
+					A master manipulator of software! You have truly conquered the art of non-conformity
+					and having code bend to your vision and will! The type to give textbook traditions
+					the middle finger and ruthlessly banish old browsers for modern up to date ones.
+					This type of developer may not sepearate their HTML and CSS content, not consider
+					their fellow team-mates in keeping their code universally easy to follow and perhaps
+					break all trust by relentless office back-stabbing on their climb to the top! 
+					Allbeit a superb and savvy developer! 	
                 </div>
 			
 	
