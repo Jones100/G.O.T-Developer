63bb0aed0343012ceb1bc43bfda051538e067c0a
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-27 19:25:20 +0100

    changed Tywin font colour and text position

--- src/testing.js
+++ src/testing.js
@@ -34,14 +34,14 @@ class Testing extends Component {
 							<div className="tywin-text-img"> </div>
 						</div>
 						<div className="tywin-text"> 
-							Another sucker for the old school...These are no suckers pardon my English, they're as tough as they come!!
-							A stern gaze and steely manner make up these folk...and usually loads of greys (full of wisdom of course). 
-							Usually found spearheading the company - on the board of directors or owning the joint, these guys are
-							tunneled in their vision and zealous in ambition! Although they do exercise tradition and are quite
-							intolerable to newer technologies/concepts like React and AI, naturally preferring remote work (as they're
-							lone wolves through and through) with a rusty desktop that does the job. They refuse to take blame or
-							extend their name alongside percieved failures as their reputation is their world! 
-							Remember to smile and be nice...we've all what happens to you in the series :-);
+							Another sucker for the old school...These are no suckers <br></br> pardon my English, they're as tough as they come!!
+							A stern gaze and steely manner make up these folk...and usually <br></br>  loads of greys (full of wisdom of course). 
+							Usually found <br></br> spearheading the company - on the board of directors or <br></br>  owning the joint, these guys are
+							tunneled in their vision <br></br> and zealous in ambition! Although they do exercise tradition and are quite
+							intolerable to newer technologies/concepts <br></br> like React and AI, naturally preferring remote work (as they're
+							lone wolves through and through) with a rusty <br></br> desktop that does the job. They refuse to take blame or <br></br>
+							extend their name alongside percieved failures as their <br></br> reputation is their world! 
+							Remember to smile and be nice...<br></br> we've all what happens to you in the series.;
 						</div>
 					</div>
 				</div>
