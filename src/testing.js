f3f30577cd12d1fbc9b524d45dc708bfae1c2085
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-29 15:18:54 +0000

    changed Varys font colour and text position

--- src/testing.js
+++ src/testing.js
@@ -87,13 +87,13 @@ class Testing extends Component {
 							<div className="varys-text-img"> </div>
 						</div>
 						<div className="varys-text"> 
-							Ohh, what an oddball indeed...these 'creatures' are found in their lairs keenly engulfing the JavaScript secrets or
-							at the rear of web seminars intently devouring info and feeding their bottomless thirst for knowledge - 
-							particularly the dark arts. Master strategists it must be said but wierdly aberrant and introverted. These developers
-							will be alone, they prefer their workmates to let them get on with it and flourish in languages such as BrainF**k and
-							C++. They're not quite as recognized for their efforts as they'd like or even deserve but their awkward social
-							display and coffee-stained ripped black hoodie are something of a repeller. They secretly wish for a friend, a hug
-							and an 'F YOU!' to the world 'if' they rise to the top...
+							Ohh, what an oddball indeed...these 'creatures' are found in <br></br> their lairs keenly engulfing the JavaScript secrets or
+							at the rear <br></br> of web seminars intently devouring info and feeding their <br></br> bottomless thirst for knowledge - 
+							particularly the dark arts. <br></br> Master strategists it must be said but wierdly aberrant and <br></br> introverted. These developers
+							will be alone, they prefer their <br></br> workmates to let them get on with it  and flourish in languages <br></br> such as BrainF**k and
+							C++. They're not quite as recognized <br></br> for their efforts as they'd like or even deserve but their awkward <br></br> social
+							display and coffee-stained ripped black hoodie are <br></br> something of a repeller. They secretly wish for a friend, a hug
+							and <br></br> an 'F YOU!' to the world 'if' they rise to the top...
 						</div>
 					</div>
 				</div>
