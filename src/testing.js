8ce7b4701940d5c384929ba0ac3695e98129bd45
Brady Jones <brady.jones@hotmail.co.uk>
2018-10-29 13:10:29 +0000

    changed Brienne font colour and positioning

--- src/testing.js
+++ src/testing.js
@@ -52,12 +52,12 @@ class Testing extends Component {
 							<div className="brienne-text-img"> 
 						</div>
 						<div className="brienne-text"> 
-							Enter Brienne...protector of the laws of web development, loyal to their teammates almost to a fault,
-							unwavering in their bold and rigid path to serve humanity and deliver clean, tried and tested code!
-							They prefer Java to JavaScript cause of it's strict and set structure, untrusting of any new technology, 
-							opting to stick to HTML 3.0 and Bootstrap 2.0! They more or less breathe depreciation! They are however
-							great in their chosen fields - whether it's client-side, server-side, full-stack etc and will get the 
-							job done! Even if it is a blast from the past and takes 40 lines of definite, solidified code rather 
+							Enter Brienne...protector of the laws of web development, <br></br> loyal to their teammates almost to a fault,
+							unwavering in <br></br> their bold and rigid path to serve humanity and deliver <br></br> clean, tried and tested code!
+							They prefer Java to <br></br> JavaScript cause of it's strict and set structure, <br></br> untrusting of any new technology, 
+							opting to stick to <br></br> HTML 3.0 and Bootstrap 2.0! They more or less <br></br> breathe depreciation! They are however
+							great in their <br></br> chosen fields - whether it's client-side, server-side, <br></br> full-stack etc and will get the 
+							job done! Even if it is <br></br> a blast from the past and takes 40 lines of definite,<br></br> solidified code rather 
 							than 20 slighly looser lines :-);
 						</div>
 					</div>
