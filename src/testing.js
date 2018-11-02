8acead09f90aefbaec5165f2a24ed89cebb59354
Brady Jones <brady.jones@hotmail.co.uk>
2018-11-02 15:25:11 +0000

    edited container sizes and got rid of absolute positions

--- src/testing.js
+++ src/testing.js
@@ -16,15 +16,15 @@ class Testing extends Component {
 						<div className="littlefinger-img-container">
 							<div className="littlefinger-text-img"> </div>
 						</div>
-						<div className="littlefinger-text"> 
-							A master manipulator of software! You have truly <br></br> conquered the art of non-conformity
-							and having code <br></br> bend to your vision and will! The type to give textbook <br></br> traditions
-							the middle finger and ruthlessly banish old <br></br> browsers for modern up to date ones.
-							This type of <br></br> developer may not sepearate their HTML and CSS <br></br> content, not consider
-							their fellow team-mates in <br></br> keeping their code universally easy to follow and <br></br> perhaps
-							break all trust by relentless office <br></br> back-stabbing on their 'climb' to the top! <br></br>
-							Allbeit a superb and savvy developer! 
-						</div>
+							<div className="littlefinger-text"> 
+								A master manipulator of software! You have truly <br></br> conquered the art of non-conformity
+								and having code <br></br> bend to your vision and will! The type to give textbook <br></br> traditions
+								the middle finger and ruthlessly banish old <br></br> browsers for modern up to date ones.
+								This type of <br></br> developer may not sepearate their HTML and CSS <br></br> content, not consider
+								their fellow team-mates in <br></br> keeping their code universally easy to follow and <br></br> perhaps
+								break all trust by relentless office <br></br> back-stabbing on their 'climb' to the top! <br></br>
+								Allbeit a superb and savvy developer! 
+							</div>
 					</div>
 				</div>
 
