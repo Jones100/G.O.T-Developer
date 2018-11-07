d5ad42ecc06b4a7237cb7fd52b68ab446f480abc
Brady Jones <brady.jones@hotmail.co.uk>
2018-11-07 19:22:34 +0000

    added individual character pages

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import "./Home.css";
 import "./Text.css";
 import Testing from "./testing.js";
 import Littlefinger from "./Littlefinger.js";
+import Tywin from "./Tywin.js";
 import Brienne from "./Brienne.js";
 import Tyrion from "./Tyrion.js";
-import Tywin from "./Tywin.js";
+import Varys from "./Varys.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
@@ -22,6 +23,11 @@ class App extends Component {
         <div className="app">
           <Route exact path="/" component={Home} />
           <Route exact path="/testing" component={Testing} />
+          <Route exact path="/littlefinger" component={Littlefinger} />
+          <Route exact path="/tywin" component={Tywin} />
+          <Route exact path="/brienne" component={Brienne} />
+          <Route exact path="/tyrion" component={Tyrion} />
+          <Route exact path="/varys" component={Varys} />
         </div>
       </Router>
     );
