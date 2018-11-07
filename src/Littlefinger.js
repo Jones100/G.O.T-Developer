9796a037efdcd2a8a3d33e70bca80cb5a8040a9f
Brady Jones <brady.jones@hotmail.co.uk>
2018-11-07 19:25:42 +0000

    created Littlefinger profile page

--- src/Littlefinger.js
+++ src/Littlefinger.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Littlefinger.css";
 
 class Littlefinger extends Component {
 	constructor(props) {
@@ -7,16 +8,28 @@ class Littlefinger extends Component {
 			};
 	}
     render() {
-			return ( 
-				<div className="littlefinger-text"> 
-					A master manipulator of software! You have truly conquered the art of non-conformity
-          			and having code bend to your vision and will! The type to give textbook traditions
-					the middle finger and ruthlessly banish old browsers for modern up to date ones.
-					This type of developer may not sepearate their HTML and CSS content, not consider
-					their fellow team-mates in keeping their code universally easy to follow and perhaps
-					break all trust by relentless office back-stabbing on their climb to the top! 
-					Allbeit a superb and savvy developer! 	
+		return (
+			<div className="character-main-container">
+
+				<div className="littlefinger-main-container">
+					<div className="img-text-container">
+						<div className="littlefinger-img-container">
+							<div className="littlefinger-text-img"> </div>
+						</div>
+						<div className="littlefinger-text">
+							A master manipulator of software! You have truly <br></br> conquered the art of non-conformity
+							and having code <br></br> bend to your vision and will! The type to give textbook <br></br> traditions
+							the middle finger and ruthlessly banish old <br></br> browsers for modern up to date ones.
+							This type of <br></br> developer may not sepearate their HTML and CSS <br></br> content, not consider
+							their fellow team-mates in <br></br> keeping their code universally easy to follow and <br></br> perhaps
+							break all trust by relentless office <br></br> back-stabbing on their 'climb' to the top! <br></br>
+							Allbeit a superb and savvy developer!
+						</div>
+					</div>
 				</div>
+
+			</div>
+
         );
     }
 }
