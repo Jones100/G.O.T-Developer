29c3afa7b513233ea537f24fbee503d0eefbfebb
Brady Jones <brady.jones@hotmail.co.uk>
2018-11-07 19:27:04 +0000

    created Brienne profile page

--- src/Brienne.js
+++ src/Brienne.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Brienne.css";
 
 class Brienne extends Component {
 	constructor(props) {
@@ -6,19 +7,30 @@ class Brienne extends Component {
 			this.state = {
 			};
 	}
-    render() {
-			return ( 
-				<div className="brienne-text"> 
-					Enter Brienne...protector of the laws of web development, loyal to their teammates almost to a fault,
-					unwavering in their bold and rigid path to serve humanity and deliver clean, tried and tested code!
-				  They prefer Java to JavaScript cause of it's strict and set structure, untrusting of any new technology, 
-					opting to stick to HTML 3.0 and Bootstrap 2.0! They more or less breathe depreciation! They are however
-					great in their chosen fields - whether it's client-side, server-side, full-stack etc and will get the 
-					job done! Even if it is a blast from the past and takes 40 lines of definite, solidified code rather 
-					than 20 slighly looser lines :-);
+	render() {
+		return ( 
+			<div className="character-main-container">
+				
+				<div className="brienne-main-container">
+					<div className="img-text-container">
+						<div className="brienne-img-container"> </div>
+							<div className="brienne-text-img">
+						</div>
+						<div className="brienne-text">
+							Enter Brienne...protector of the laws of web development, loyal <br></br> to their teammates almost to a fault,
+							unwavering in their bold <br></br> &#160; and rigid path to serve humanity and deliver clean, tried <br></br> &#160; and tested code!
+							They prefer Java to JavaScript cause <br></br> &#160; of it's strict and set structure, untrusting of any new <br></br> &#160;&#160; &#160; technology,
+							opting to stick to HTML 3.0 and <br></br> &#160; &#160;&#160; &#160;&#160; &#160; Bootstrap 2.0! They more or less breathe <br></br>
+							&#160; &#160;&#160;&#160;&#160; &#160;&#160; depreciation! They are however great in their <br></br> &#160; &#160;&#160;&#160;&#160;&#160;&#160;&#160; &#160;&#160; &#160; chosen fields -
+							whether it's client-side, <br></br>&#160; &#160;&#160; &#160;&#160; &#160;&#160;&#160;&#160; server-side,full-stack etc and will get the job <br></br> &#160; &#160;&#160; &#160;&#160; &#160;&#160; &#160; done! Even if it is a blast from the past and
+							<br></br> &#160; &#160;&#160; &#160;&#160; &#160;&#160; &#160;&#160;&#160;&#160; takes 40 lines of definite, solidified code <br></br>&#160; &#160;&#160; &#160;&#160; &#160;&#160; &#160;&#160; &#160;&#160;&#160; rather than 20 slighly looser lines :-);
+						</div>
+					</div>
 				</div>
-        );
-    }
+
+			</div>
+		);
+	}
 }
 
 export default Brienne;
