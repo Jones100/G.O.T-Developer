09c2d57468380be67d3b2a3925fd410e7cd5c22f
Brady Jones <brady.jones@hotmail.co.uk>
2018-11-13 10:46:58 +0000

    added Dragon image and Dragon pages

---
+++ src/Dragon.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./Dragon.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Dragon extends Component {
+	constructor(props) {
+		super(props);
+			this.state = {
+			};
+	}
+    render() {
+		return (
+            <div className="dragon-main-page-container">
+                <div className="dragon-img-container">
+                    <div className="dragon"> </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Dragon;
