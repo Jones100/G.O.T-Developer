155ee2f585327b902922dcec8a129304c1e04ba3
Brady Jones <brady.jones@hotmail.co.uk>
2018-11-20 18:25:02 +0000

    added character database page

---
+++ src/Database.js
@@ -0,0 +1,7 @@
+let characterDatabase = {
+    "Littlefinger": {
+        icon: "littlefinger.jpg", profile: "A master manipulator of software! You have truly conquered the art of non-conformity and having code bend to your vision and will! The type to give textbook traditions the middle finger and ruthlessly banish old browsers for modern up to date ones. This type of developer may not sepearate their HTML and CSS content, not consider their fellow team-mates in keeping their code universally easy to follow and perhaps break all trust by relentless office back-stabbing on their climb to the top! Allbeit a superb and savvy developer!"
+    },
+
+
+};
