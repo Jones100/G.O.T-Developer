113e89fcfc75407a4f541fbcf13df8cce05e1b4d
Brady Jones <brady.jones@hotmail.co.uk>
2018-11-24 02:34:17 +0000

    deleted Profile component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import Home from "./Home";
 import Dragon from "./Dragon";
-import Profile from "./Profile";
 import Littlefinger from "./Littlefinger";
 import Tywin from "./Tywin";
 import Brienne from "./Brienne";
@@ -22,7 +21,6 @@ class App extends Component {
         <div className="app">
           <Route exact path="/" component={Home} />
           <Route exact path="/dragon" component={Dragon} />
-          <Route exact path="/profile/:character" component={Profile} />
           <Route exact path="/littlefinger" component={Littlefinger} />
           <Route exact path="/tywin" component={Tywin} />
           <Route exact path="/brienne" component={Brienne} />
